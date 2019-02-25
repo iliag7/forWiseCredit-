b6c54e88306bded03476f7697fadf3b6d30ce681
iliag7 <iliag777@gmail.com>
2019-02-25 17:08:36 +0200

    Add files via upload

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,1 +1,10 @@
+var mysql = require('mysql');
 
+var pool = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'db_newtest'
+});
+
+module.exports = pool;
